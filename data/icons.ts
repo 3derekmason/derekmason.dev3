d62b32d345c64ff6b9efc7b245728f21c429205f
3derekmason <3derekmason@gmail.com>
2026-07-16 11:03:41 -0600

    feat: scaffold about page

--- data/icons.ts
+++ data/icons.ts
@@ -9,6 +9,6 @@ export const icons = {
     "arcade": "M2 5h20v14H2zm18 12V7H4v10zM8 9h2v2h2v2h-2v2H8v-2H6v-2h2zm6 0h2v2h-2zm4 4h-2v2h2z",
     "user": "M15 2H9v2H7v6h2V4h6zm0 8H9v2h6zm0-6h2v6h-2zM4 16h2v-2h12v2H6v4h12v-4h2v6H4z",
     "menu": "M4 6h16v2H4zm0 5h16v2H4zm16 5H4v2h16z",
-    "back": "M8 13v-2h2v2H8Zm2-2V9h2v2h-2Zm0 4v-2h2v2h-2Zm2-6V7h2v2h-2Zm0 8v-2h2v2h-2Zm2-10V5h2v2h-2Zm0 12v-2h2v2h-2Z"
-
-}
+    "back": "M8 13v-2h2v2H8Zm2-2V9h2v2h-2Zm0 4v-2h2v2h-2Zm2-6V7h2v2h-2Zm0 8v-2h2v2h-2Zm2-10V5h2v2h-2Zm0 12v-2h2v2h-2Z",
+    "hello": "M19 23H7v-2h12v2ZM7 21H5v-2h2v2Zm14 0h-2v-2h2v2ZM5 19H3v-2h2v2Zm18 0h-2V7h2v12ZM3 17H1v-4h2v4Zm6 0H7v-2h2v2Zm-2-2H5v-2H3v-2h2V5h2v10Zm14-8h-2v5h-2V5h4v2Zm-10 4H9V5H7V3h4v8Zm6-6h-2v6h-2V3h4v2Zm-4-2h-2V1h2v2Z"
+};
--- pages/about.vue
+++ pages/about.vue
@@ -1,14 +1,56 @@
 <template>
-  <div class="flex flex-col w-full max-w-[800px] h-full">
-    <span class="flex gap-4 items-center py-4">
-      <AppIcon name="user" :width="48" :height="48" />
-      <p class="text-3xl">Hi, I'm Derek!</p>
+  <div
+    class="flex flex-col w-full max-w-[800px] h-full max-h-screen md:mt-10 gap-8"
+  >
+    <span class="flex flex-col gap-6 md:items-center md:flex-row">
+      <AppIcon name="hello" :width="48" :height="48" class="wave" />
+      <p class="leading-8 md:text-lg">
+        Hi, I'm Derek, a software developer who took a slightly unconventional
+        route into tech.
+      </p>
     </span>
-    <p class="leading-8 md:text-lg">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid in
-      mollitia illo aliquam quia, voluptas esse eaque eveniet deserunt rem
-      temporibus itaque omnis alias quas assumenda, voluptatum nulla, optio
-      ullam?
+    <p class="leading-8 md:text-lg md:pl-[62px]">
+      Before switching to code, I worked as a preschool teacher and a personal
+      trainer. Both of which taught me a lot about patience, communication,
+      problem solving, and empathy. A few years ago, I made the leap into
+      software development and have been building ever since.
+    </p>
+    <p class="leading-8 md:text-lg md:pl-[62px]">
+      These days, I work primarily on web applications, but I still love the
+      same things that drew me to tech in the first place... Figuring out how
+      things work behind the scenes, solving interesting and unique problems,
+      and turning an idea into something real.
+    </p>
+    <p class="leading-8 md:text-lg md:pl-[62px]">
+      Outside of coding, my biggest passions are music, family, friends, and
+      health.
     </p>
   </div>
 </template>
+
+<style scoped>
+.wave {
+  animation: wave 2s infinite;
+}
+
+@keyframes wave {
+  0% {
+    transform: rotate(0deg);
+  }
+  15% {
+    transform: rotate(15deg);
+  }
+  25% {
+    transform: rotate(10deg);
+  }
+  35% {
+    transform: rotate(15deg);
+  }
+  50% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+}
+</style>
