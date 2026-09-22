08f2896b5e0fae09a7e24d1e2e7fac8c719d6df1
3derekmason <3derekmason@gmail.com>
2026-09-22 09:46:41 -0600

    edit about page blurb and add social links

--- pages/about.vue
+++ pages/about.vue
@@ -1,30 +1,62 @@
+<script setup lang="ts">
+const title = ref("Hello!");
+const splitTitle = ref(title.value.split(""));
+const currentTitle = ref("");
+
+onMounted(() => {
+  setInterval(() => {
+    currentTitle.value += splitTitle.value.shift() || "";
+  }, 100);
+});
+</script>
+
 <template>
   <div
     class="flex flex-col w-full max-w-[800px] h-full max-h-screen md:mt-10 gap-8"
   >
     <span class="flex flex-col gap-6 md:items-center md:flex-row">
-      <AppIcon name="hello" :width="48" :height="48" class="wave" />
-      <p class="leading-8 md:text-lg">
-        Hi, I'm Derek, a software developer who took a slightly unconventional
-        route into tech.
-      </p>
+      <!-- <AppIcon name="hello" :width="48" :height="48" class="wave" /> -->
+      <p class="leading-8 md:text-2xl">{{ currentTitle }}</p>
     </span>
-    <p class="leading-8 md:text-lg md:pl-[62px]">
-      Before switching to code, I worked as a preschool teacher and a personal
-      trainer. Both of which taught me a lot about patience, communication,
-      problem solving, and empathy. A few years ago, I made the leap into
-      software development and have been building ever since.
+    <p class="leading-8 md:text-lg md:pl-10">
+      I'm Derek, a software developer, music enthusiast, and lifelong learner.
+      Formerly I worked as a preschool teacher and a personal trainer. I build
+      to try and impact people's lives in a positive way. I have always been
+      interested in creating things - music, games, workouts, websites, some
+      crummy art - while learning as much as I can through the process.
+      Nowadays, when I'm not behind a computer screen I'm probably enjoying some
+      music, exploring rivers, lakes, and mountains, or entertaining my slight
+      obsession with my favorite basketball team. (Go Sixers!)
     </p>
-    <p class="leading-8 md:text-lg md:pl-[62px]">
-      These days, I work primarily on web applications, but I still love the
-      same things that drew me to tech in the first place... Figuring out how
-      things work behind the scenes, solving interesting and unique problems,
-      and turning an idea into something real.
-    </p>
-    <p class="leading-8 md:text-lg md:pl-[62px]">
-      Outside of coding, my biggest passions are music, family, friends, and
-      health.
-    </p>
+    <div class="flex flex-col gap-4">
+      <p class="leading-8 md:text-lg">Get in touch:</p>
+      <div class="flex flex-col gap-4 md:gap-8 md:flex-row md:pl-10">
+        <div class="flex gap-2 items-center">
+          <AppIcon name="mail" :width="24" :height="24" />
+          <a href="mailto:me@derekmason.dev" class="text-primary-500"
+            >me@derekmason.dev</a
+          >
+        </div>
+        <div class="flex gap-2 items-center">
+          <AppIcon name="github" :width="24" :height="24" />
+          <a
+            href="https://github.com/3derekmason"
+            target="_blank"
+            class="text-primary-500"
+            >Github</a
+          >
+        </div>
+        <div class="flex gap-2 items-center">
+          <AppIcon name="linkedin" :width="24" :height="24" />
+          <a
+            href="https://www.linkedin.com/in/3derekmason"
+            target="_blank"
+            class="text-primary-500"
+            >LinkedIn</a
+          >
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
